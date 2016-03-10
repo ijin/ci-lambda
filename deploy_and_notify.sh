f7e326f36edcceca346ecd9b7b344d0c2f213c3d
Michael H. Oshita <ijinpublic+github@gmail.com>
2016-03-10 22:40:48 +0900

    set exit code

--- deploy_and_notify.sh
+++ deploy_and_notify.sh
@@ -13,19 +13,21 @@ elif [ $1 == 'production' ]; then
     export S3_BUCKET=$S3_BUCKET_PRODUCTION
     export LAMBDA_EXEC_ROLE=$LAMBDA_EXEC_ROLE_PRODUCTION
     python src/environment.py production
-    pushd src && . env.sh production; python install.py && popd
+    pushd src && . env.sh ; python install.py && popd
 fi
 
 if [ $? -eq 0 ]; then
     export SL_COLOR="good"
     export SL_TEXT="Success: Deployed ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
     export SL_ICON="https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2015-05-21/4984475810_da919f053253801b5ed9_34.jpg"
+    export EXIT=0
 else
     export SL_COLOR="danger"
     export SL_TEXT="Failure: Deploying ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
     export SL_ICON="https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2015-05-21/4984475810_da919f053253801b5ed9_34.jpg"
+    export EXIT=1
 fi
 
 curl -X POST --data-urlencode 'payload={"username": "One Garden", "icon_url": "'"$SL_ICON"'", "channel": "'"${CHANNEL:-#test}"'", "attachments": [{ "color": "'"$SL_COLOR"'", "text": "'"$SL_TEXT"'", "mrkdwn_in": ["text"] }] }' https://hooks.slack.com/services/${SLACK_HOOK}
 
-
+exit $EXIT
